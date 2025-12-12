26151e59f5d258d602e1c5d5f63e2730a61aa729
Artem <killer1234790@gmail.com>
2025-12-12 14:37:21 +0300

    Init: init mono-repos for project

---
+++ src/client/UnityClient/DiceRaceClient/Assets/src/test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using GameCore;
+public class test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var t = new Class1();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
